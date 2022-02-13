a7ad9e322b906b3125c371ac9810585caee45492
T-J Hayward <Trinity-Jayne.Hayward@ibm.com>
2022-02-13 23:28:54 +0000

    last chance mr pipeline

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import "../styles/globals.css";
-import { AuthContextProvider } from "../stores/authContext";
+
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthContextProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
